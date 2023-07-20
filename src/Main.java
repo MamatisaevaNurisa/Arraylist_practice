21ad32435f8df76bfcdbf99738c8c541bc90b677
method <mamatisaevanurisa@icloud.com>
2023-07-20 15:47:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        ArrayList<Integer> words = new ArrayList<>();
+//         int [] sandardynmassivi = new int[10];
+//         sandardynmassivi[0]=1;
+//         sandardynmassivi[1]=1;
+//         sandardynmassivi[2]=1;
+//         sandardynmassivi[3]=1;
+//         sandardynmassivi[4]=1;
+//         sandardynmassivi[5]=1;
+//         sandardynmassivi[6]=1;
+//         sandardynmassivi[7]=1;
+//         sandardynmassivi[8]=1;
+//         sandardynmassivi[9]=1;
+//         sandardynmassivi[10]=1;
+//
+//            words.add(10);
+//        System.out.println(words.size());
+//        words.add(2);
+//        System.out.println(words.size());
+
+//
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add("hello");
+        words.add("world");
+        words.add("!");
+        System.out.println(words.indexOf("!"));
+        words.add(3,"my name is Nurisa");
+        words.clear();
+        System.out.println(words.size());
+
+
+//        for (int i = 0; i < size; i++) {
+//            System.out.println(words.get(i));
+//        }
+//
+//        System.out.println(words.get(3));
+//
+
+
+
+
+
+
+    }
+}
